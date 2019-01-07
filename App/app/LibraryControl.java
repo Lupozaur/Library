a57433040f11b92bc3aabe0b9647ba5798bbcdb8
lenovo <lenovo@DESKTOP-JBSQJQC>
2019-01-07 18:10:41 +0100

    Usunięcie Option, dodanie jako klasa wewnętrzna w LibraryControl

--- App/app/LibraryControl.java
+++ App/app/LibraryControl.java
@@ -84,4 +84,36 @@ public class LibraryControl {
 		LibraryUtils.printBooks(library);
 	}
 
+	private enum Option {
+
+		EXIT(0, "Wyjście z programu"), 
+		ADD_BOOK(1, "Dodanie książki"), 
+		ADD_MAGAZINE(2, "Dodanie książki"),
+		PRINT_BOOKS(3, "Wyświetlanie dostępnych książek"), 
+		PRINT_MAGAZINES(4, "Wyświetlanie dostępnych czasopism");
+
+		private int value;
+		private String description;
+
+		Option(int value, String description) {
+			this.value = value;
+			this.description = description;
+		}
+
+		@Override
+		public String toString() {
+			// TODO Auto-generated method stub
+			return value + " - " + description;
+		}
+
+		public static Option createFromInt(int option) throws NoSuchElementException {
+			Option result = null;
+			try {
+				result = Option.values()[option];
+			} catch (ArrayIndexOutOfBoundsException e) {
+				throw new NoSuchElementException("Brak elementu o wskazanym ID");
+			}
+			return result;
+		}
+	}
 }
